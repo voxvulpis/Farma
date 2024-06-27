fcd705f4eca8d6d82d90c5144c30d22b0c3a79b2
Edu Teodoro <edu.teodoro@tebesensor.com>
2024-06-27 11:53:55 -0300

    new Utils

---
+++ src/main/java/br/unesp/farma/utils/ClientUtils.java
@@ -0,0 +1,16 @@
+package br.unesp.farma.utils;
+
+public class ClientUtils {
+
+    public static void createClient(){
+
+    }
+
+    public static void editClient(){
+
+    }
+
+    public static void deleteClient(){
+
+    }
+}
---
+++ src/main/java/br/unesp/farma/utils/EmployeeUtils.java
@@ -0,0 +1,19 @@
+package br.unesp.farma.utils;
+
+import br.unesp.farma.models.Employee;
+
+public class EmployeeUtils {
+
+    public static void createEmployee(){
+        Employee employee = new Employee();
+
+    }
+
+    public static void editEmployee(){
+
+    }
+
+    public static void deleteEmployee(){
+
+    }
+}
