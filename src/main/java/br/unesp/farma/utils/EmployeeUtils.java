83c515fc6e820bfe8ffe30b89f4cda334951800a
Edu Teodoro <edu.teodoro@tebesensor.com>
2024-06-27 15:44:15 -0300

    new Utils

--- src/main/java/br/unesp/farma/utils/EmployeeUtils.java
+++ src/main/java/br/unesp/farma/utils/EmployeeUtils.java
@@ -1,12 +1,12 @@
 package br.unesp.farma.utils;
 
 import br.unesp.farma.models.Employee;
+import br.unesp.farma.models.Role;
 
 public class EmployeeUtils {
 
-    public static void createEmployee(){
-        Employee employee = new Employee();
-
+    public static void createEmployee(Role role){
+        Employee employee = new Employee(role);
     }
 
     public static void editEmployee(){
